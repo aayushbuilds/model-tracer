8f1df53ed3d320e5d174fab362626461f22a9422
Aayush Gautam <53272422+L65FREAD@users.noreply.github.com>
2025-09-03 21:44:34 -0500

    feat: removed about

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import ModelDetail from './pages/ModelDetail';
-import {About} from "@/pages/About.tsx";
 
 function App() {
     return (
@@ -9,7 +8,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/model/:id" element={<ModelDetail/>}/>
-                <Route path="/about" element={<About/>}/>
             </Routes>
         </Router>
     );
